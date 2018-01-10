1d797c329bbb86859e5c8bf77543997c7f2ecd29
SonnyAasen <35238145+SonnyAasen@users.noreply.github.com>
2018-01-10 13:42:25 +0100

    Read index file as bytes directly

--- Webbserver/Webbserver/Program.cs
+++ Webbserver/Webbserver/Program.cs
@@ -39,30 +39,31 @@ namespace Webbserver
                 HttpListenerRequest request = context.Request;
                 // Obtain a response object.
                 HttpListenerResponse response = context.Response;
-                
+                context.Response.ContentType = "text/html";
+
                 response.AddHeader("Content-Type" , "text/html");
-                response.AddHeader("Content-Type", "image/jpeg");
+                
                 // Construct a response.
                 string reqed = request.RawUrl;
-                string test = File.ReadAllText(@"..\..\..\..\Content" + reqed);
-                byte[] image = File.ReadAllBytes(@"..\..\..\..\Content" + @"\laughing_panda.jpg");
-                string responseString = test;                
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                byte[] buffer = File.ReadAllBytes(@"..\..\..\..\Content" + reqed);
+                
+               // string responseString = test;                
+               // byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
 
 
 
+               
 
 
                 // Get a response stream and write the response to it.
-                response.ContentLength64 = buffer.Length + image.Length;
+                response.ContentLength64 = buffer.Length;
                // response.ContentLength64 = image.Length;
                 System.IO.Stream output = response.OutputStream;
                 output.Write(buffer, 0, buffer.Length);
                 
                 // You must close the output stream.
 
-                output.Write(image, 0, image.Length);
-
+               
                 output.Close();
                 listener.Stop();
             }
