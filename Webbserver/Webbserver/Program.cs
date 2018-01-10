6f3f7d7dfce30cb9ff8b9d2b000f5baecdbfe6b3
mmmolin78 <mmmolin78@gmail.com>
2018-01-10 09:13:23 +0100

    added rawurl

--- Webbserver/Webbserver/Program.cs
+++ Webbserver/Webbserver/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -11,43 +12,43 @@ namespace Webbserver
     {
         static void Main(string[] prefixes)
         {
-            while(true)
+            while (true)
             {
-
-            if (!HttpListener.IsSupported)
-            {
-                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
-                return;
-            }
-            // URI prefixes are required,
-            // for example "http://contoso.com:8080/index/".
-            if (prefixes == null || prefixes.Length == 0)
-                throw new ArgumentException("prefixes");
-
-            // Create a listener.
-            HttpListener listener = new HttpListener();
-            // Add the prefixes.
-            foreach (string s in prefixes)
-            {
-                listener.Prefixes.Add(s);
-            }
-            listener.Start();
-            Console.WriteLine("Listening...");
-            // Note: The GetContext method blocks while waiting for a request. 
-            HttpListenerContext context = listener.GetContext();
-            HttpListenerRequest request = context.Request;
-            // Obtain a response object.
-            HttpListenerResponse response = context.Response;
-            // Construct a response.
-            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-            // Get a response stream and write the response to it.
-            response.ContentLength64 = buffer.Length;
-            System.IO.Stream output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            // You must close the output stream.
-            output.Close();
-            listener.Stop();
+                if (!HttpListener.IsSupported)
+                {
+                    Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                    return;
+                }
+                // URI prefixes are required,
+                // for example "http://contoso.com:8080/index/".
+                if (prefixes == null || prefixes.Length == 0)
+                    throw new ArgumentException("prefixes");
+                // Create a listener.
+                HttpListener listener = new HttpListener();
+                // Add the prefixes.
+                foreach (string s in prefixes)
+                {
+                    listener.Prefixes.Add(s);
+                }
+                listener.Start();
+                Console.WriteLine("Listening...");
+                // Note: The GetContext method blocks while waiting for a request. 
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                string reqed = request.RawUrl;
+                string test = File.ReadAllText(@"..\..\..\..\Content" + reqed);
+                string responseString = test;                
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+                listener.Stop();
             }
         }
     }
