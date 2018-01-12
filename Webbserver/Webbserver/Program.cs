16dcd1bac17636466a61cac199b68835f11a26ee
mmmolin78 <mmmolin78@gmail.com>
2018-01-12 12:24:55 +0100

    cleaned upp some stuff

--- Webbserver/Webbserver/Program.cs
+++ Webbserver/Webbserver/Program.cs
@@ -13,81 +13,7 @@ namespace Webbserver
     class Program
     {
         static string requested = "";
-        public static void SetResponseContentType(string requestedFile, HttpListenerResponse response, HttpListenerRequest request)
-        {
-            DateTime now = DateTime.Now;
-            DateTime future = now.AddYears(1);
-            string expires = future.ToString();
-
-            response.AddHeader("Expires", expires);
-            if (requestedFile.EndsWith(".html"))
-            {
-                Console.WriteLine("Response was content type text/html");
-                response.AddHeader("Content-Type", "text/html");
-
-            }
-            else if (requestedFile.EndsWith(".htm"))
-            {
-                Console.WriteLine("Response was content type text/html");
-                response.AddHeader("Content-Type", "text/html");
-            }
-            else if (requestedFile.EndsWith(".jpg"))
-            {
-                Console.WriteLine("Response was content type image/jpeg");
-                response.AddHeader("Content-Type", "image/jpeg");
-            }
-            else if (requestedFile.EndsWith(".gif"))
-            {
-                Console.WriteLine("Response was content type image/gif");
-                response.AddHeader("Content-Type", "image/gif");
-            }
-            else if (requestedFile.EndsWith(".pdf"))
-            {
-                Console.WriteLine("Response was content type application/pdf");
-                response.AddHeader("Content-Type", "application/pdf");
-            }
-            else if (requestedFile.EndsWith(".js"))
-            {
-                Console.WriteLine("Response was content type application/x-javascript");
-                response.AddHeader("Content-Type", "application/x-javascript");
-            }
-
-            else if (requestedFile.EndsWith(".css"))
-            {
-                Console.WriteLine("Response was content type css");
-                response.AddHeader("Content-Type", "text/css");
-            }
-
-            else if (requestedFile == "/Subfolder/")
-            {
-                response.ContentType = "text/html";
-                requested = @"\Subfolder\index.html";
-            }
-            else if (requestedFile.Contains("/Dynamic"))
-            {
-                if ((request.QueryString["input1"] != null) && (request.QueryString["input1"] != null))
-                {
-                    string[] splitQuery = new string[2];
-                    splitQuery[0] = request.QueryString["input1"];
-                    splitQuery[1] = request.QueryString["input2"];
-                    int input1 = int.Parse(splitQuery[0]);
-                    int input2 = int.Parse(splitQuery[1]);
-                    
-                    int result = input1 + input2;
-                    
-                    string dynamicPage = "<HTML><BODY>" + result.ToString() + "</BODY></HTML>";
-
-                    byte[] buffer = Encoding.UTF8.GetBytes(dynamicPage);
-
-                    response.ContentLength64 = buffer.Length;
-                    System.IO.Stream output = response.OutputStream;
-                    output.Write(buffer, 0, buffer.Length);
-
-                    output.Close();
-                }
-
-            }
-        }
+        
         static void Main(string[] prefixes)
         {
             if (!HttpListener.IsSupported)
@@ -110,9 +36,7 @@ namespace Webbserver
             }
             listener.Start();
             Console.WriteLine("Listening...");
-
-            //Dictionary<string, int> webServerUserCookie = new Dictionary<string, int>();
-            //webServerUserCookie.Add("counter1", counter);
+            
             Dictionary<string, int> sessionCounters = new Dictionary<string, int>();
 
             while (true)
@@ -167,7 +91,6 @@ namespace Webbserver
                     SetResponseContentType(requested, response, request);
                     if (File.Exists(@"..\..\..\..\Content" + requested))
                     {
-
                         byte[] buffer = File.ReadAllBytes(@"..\..\..\..\Content" + requested);
                         // Get a response stream and write the response to it.
                         response.ContentLength64 = buffer.Length;
@@ -190,15 +113,79 @@ namespace Webbserver
                             output.Close();
                         }
                     }
-
-                }
-
-                // Respond with correct Content-Type
-
-
-                // You must close the output stream.
-
+                }               
                 //  listener.Stop();
+            }
+        }
+        public static void SetResponseContentType(string requestedFile, HttpListenerResponse response, HttpListenerRequest request)
+        {
+            DateTime now = DateTime.Now;
+            DateTime future = now.AddYears(1);
+            string expires = future.ToString();
+            response.AddHeader("Expires", expires);
+
+            if (requestedFile.EndsWith(".html"))
+            {
+                Console.WriteLine("Response was content type text/html");
+                response.AddHeader("Content-Type", "text/html");
+            }
+            else if (requestedFile.EndsWith(".htm"))
+            {
+                Console.WriteLine("Response was content type text/html");
+                response.AddHeader("Content-Type", "text/html");
+            }
+            else if (requestedFile.EndsWith(".jpg"))
+            {
+                Console.WriteLine("Response was content type image/jpeg");
+                response.AddHeader("Content-Type", "image/jpeg");
+            }
+            else if (requestedFile.EndsWith(".gif"))
+            {
+                Console.WriteLine("Response was content type image/gif");
+                response.AddHeader("Content-Type", "image/gif");
+            }
+            else if (requestedFile.EndsWith(".pdf"))
+            {
+                Console.WriteLine("Response was content type application/pdf");
+                response.AddHeader("Content-Type", "application/pdf");
+            }
+            else if (requestedFile.EndsWith(".js"))
+            {
+                Console.WriteLine("Response was content type application/x-javascript");
+                response.AddHeader("Content-Type", "application/x-javascript");
+            }
+            else if (requestedFile.EndsWith(".css"))
+            {
+                Console.WriteLine("Response was content type css");
+                response.AddHeader("Content-Type", "text/css");
+            }
+            else if (requestedFile == "/Subfolder/")
+            {
+                response.ContentType = "text/html";
+                requested = @"\Subfolder\index.html";
+            }
+            else if (requestedFile.Contains("/Dynamic"))
+            {
+                if ((request.QueryString["input1"] != null) && (request.QueryString["input1"] != null))
+                {
+                    string[] splitQuery = new string[2];
+                    splitQuery[0] = request.QueryString["input1"];
+                    splitQuery[1] = request.QueryString["input2"];
+                    int input1 = int.Parse(splitQuery[0]);
+                    int input2 = int.Parse(splitQuery[1]);
+
+                    int result = input1 + input2;
+
+                    string dynamicPage = "<HTML><BODY>" + result.ToString() + "</BODY></HTML>";
+
+                    byte[] buffer = Encoding.UTF8.GetBytes(dynamicPage);
+
+                    response.ContentLength64 = buffer.Length;
+                    System.IO.Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+
+                    output.Close();
+                }
             }
         }
     }
